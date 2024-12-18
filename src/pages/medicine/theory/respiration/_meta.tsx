8ea5cf9d96662253042b37c794ffbdbcf792237c
whitedark <blancooscuro.mm@gmail.com>
2024-12-18 09:33:25 +0630

    asthma

--- src/pages/medicine/theory/respiration/_meta.tsx
+++ src/pages/medicine/theory/respiration/_meta.tsx
@@ -1,4 +1,6 @@
 export default {
+  asthma: "Asthma",
+  "acute-asthma": "Acute Exacerbations of Asthma",
   copd: "Chronic Obstructive Pulmonary Disease",
   aecopd: "Acute Exacerbations of COPD",
 };
