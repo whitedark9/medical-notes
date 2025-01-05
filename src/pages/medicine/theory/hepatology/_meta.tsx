efbf10d9ccd845acd5d2e9b4e4a6351b1260d71c
whitedark9 <blancooscuro.mm@gmail.com>
2025-01-06 00:27:15 +0630

    gi

--- src/pages/medicine/theory/hepatology/_meta.tsx
+++ src/pages/medicine/theory/hepatology/_meta.tsx
@@ -1,3 +1,7 @@
 export default {
   "cirrhosis-of-liver": "Cirrhosis of Liver",
+  "hepatic-encephalopathy": "Hepatic Encephalopathy",
+  jaundice: "Jaundice",
+  ascites: "Ascites",
+  "gastrointestinal-bleeding": "Gastrointestinal Bleeding",
 };
