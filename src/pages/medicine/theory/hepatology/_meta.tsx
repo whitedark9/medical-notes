6d5c2fad86b1716ffea1ce9b8a1326cfa2f7c64a
whitedark9 <blancooscuro.mm@gmail.com>
2025-01-19 19:57:41 +0630

    ph

--- src/pages/medicine/theory/hepatology/_meta.tsx
+++ src/pages/medicine/theory/hepatology/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   "cirrhosis-of-liver": "Cirrhosis of Liver",
+  "portal-hypertension": "Portal Hypertension",
   "hepatic-encephalopathy": "Hepatic Encephalopathy",
   jaundice: "Jaundice",
   ascites: "Ascites",
