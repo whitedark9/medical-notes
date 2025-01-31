13f02f08e91076e90c4f45ee6c6e0913fa86d39d
whitedark9 <blancooscuro.mm@gmail.com>
2025-01-31 22:46:02 +0630

    up

--- src/pages/medicine/theory/hepatology/_meta.tsx
+++ src/pages/medicine/theory/hepatology/_meta.tsx
@@ -2,7 +2,5 @@ export default {
   "cirrhosis-of-liver": "Cirrhosis of Liver",
   "portal-hypertension": "Portal Hypertension",
   "hepatic-encephalopathy": "Hepatic Encephalopathy",
-  jaundice: "Jaundice",
   ascites: "Ascites",
-  "gastrointestinal-bleeding": "Gastrointestinal Bleeding",
 };
--- src/pages/medicine/theory/respiration/_meta.tsx
+++ src/pages/medicine/theory/respiration/_meta.tsx
@@ -2,5 +2,5 @@ export default {
   asthma: "Bronchial Asthma",
   "acute-asthma": "Acute Exacerbations of Bronchial Asthma",
   copd: "Chronic Obstructive Pulmonary Disease",
-  aecopd: "Acute Exacerbations of COPD",
+  "pleural-effusion": "Pleural Effusion",
 };
