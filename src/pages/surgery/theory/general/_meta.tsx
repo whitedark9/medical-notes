3829af715ab0b50cea8ac69b446d755d1a33bacf
whitedark9 <blancooscuro.mm@gmail.com>
2025-03-02 22:32:16 +0630

    tet

--- src/pages/surgery/theory/general/_meta.tsx
+++ src/pages/surgery/theory/general/_meta.tsx
@@ -9,4 +9,6 @@ export default {
   "who-surgical-safety-checklist": "WHO Surgical Safety Checklist",
   "post-operative-fever": "Post-Op Fever",
   "post-operative-complications": "Post-Op Complications",
+  "surgery-in-malignancy": "Surgery in Malignancy",
+  tetanus: "Tetanus",
 };
