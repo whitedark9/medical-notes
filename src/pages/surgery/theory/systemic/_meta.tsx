d0ca36c5e9fed6c3c4e3117e27da5a3244e09889
whitedark9 <blancooscuro.mm@gmail.com>
2025-03-17 22:14:07 +0630

    up

--- src/pages/surgery/theory/systemic/_meta.tsx
+++ src/pages/surgery/theory/systemic/_meta.tsx
@@ -3,4 +3,6 @@ export default {
   goiter: "Goiter",
   "graves-disease": "Graves' Disease",
   solitary: "Solitary Thyroid Nodule",
+  goo: "Gastric Outlet Obstruction",
+  "gastric-cancer": "Gastric Cancer",
 };
