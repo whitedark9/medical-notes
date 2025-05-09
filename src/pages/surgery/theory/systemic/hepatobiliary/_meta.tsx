3f1668bc70645619868d517bdc8bc1a9de9c9726
whitedark9 <blancooscuro.mm@gmail.com>
2025-05-09 08:59:41 +0630

    up

---
+++ src/pages/surgery/theory/systemic/hepatobiliary/_meta.tsx
@@ -0,0 +1,7 @@
+export default {
+  "surgical-jaundice": "Surgical Jaundice",
+  "oesophageal-varices": "Oesophageal Varices",
+  hcc: "Hepatocellular Carcinoma",
+  choledocholithiasis: "Choledocholithiasis",
+  "courvoisier-law": "Courvoisier's Law",
+};
