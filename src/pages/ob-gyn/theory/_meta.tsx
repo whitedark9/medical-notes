9edcbd0544989fd32e1977071e045a7542ad164a
whitedark9 <blancooscuro.mm@gmail.com>
2025-06-13 00:42:10 +0630

    up

--- src/pages/ob-gyn/theory/_meta.tsx
+++ src/pages/ob-gyn/theory/_meta.tsx
@@ -1,6 +1,7 @@
 export default {
   index: "Introduction",
   "an-care": "Antenatal Care",
+  complications: "Pregnancy Complications",
   "medical-disorders": "Medical Disorders",
   labour: "Labour",
 };
--- src/pages/ob-gyn/theory/labour/_meta.tsx
+++ src/pages/ob-gyn/theory/labour/_meta.tsx
@@ -1,4 +1,6 @@
 export default {
   vbac: "Vaginal Birth After CS",
   "breech-presentation": "Breech Presentation",
+  "transverse-lie": "Transverse Lie",
+  "op-position": "OP Position",
 };
