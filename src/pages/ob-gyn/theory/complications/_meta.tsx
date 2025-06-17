935c38091547d9d477a48135faae3d440f014039
whitedark9 <blancooscuro.mm@gmail.com>
2025-06-17 18:20:02 +0630

    up

--- src/pages/ob-gyn/theory/complications/_meta.tsx
+++ src/pages/ob-gyn/theory/complications/_meta.tsx
@@ -2,4 +2,5 @@ export default {
   polyhydramnios: "Polyhydramnios",
   "placenta-previa": "Placenta Previa",
   "abruptio-placenta": "Abruptio Placenta",
+  "multiple-pregnancy": "Multiple Pregnancy",
 };
