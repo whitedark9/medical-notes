2e1f8bab231e2148edf03b4d06fb97b4c3466c7e
whitedark9 <blancooscuro.mm@gmail.com>
2025-06-20 21:01:54 +0630

    up

--- src/pages/pediatrics/_meta.tsx
+++ src/pages/pediatrics/_meta.tsx
@@ -1,9 +1,4 @@
 export default {
-  // "*": {
-  //   theme: {
-  //     layout: "default",
-  //   },
-  // },
   index: "Introduction",
   bedside: "Bedside",
   theory: "Theory",
--- src/pages/pediatrics/theory/_meta.tsx
+++ src/pages/pediatrics/theory/_meta.tsx
@@ -1,5 +1,4 @@
 export default {
-  index: "Introduction",
   paper1: "Paper 1",
   paper2: "Paper 2",
 };
