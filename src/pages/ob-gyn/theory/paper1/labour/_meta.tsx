22eea56d66e3c83885bdeb5fb1916f824a5423f1
whitedark9 <blancooscuro.mm@gmail.com>
2025-07-01 00:08:39 +0630

    up

--- src/pages/ob-gyn/theory/paper1/labour/_meta.tsx
+++ src/pages/ob-gyn/theory/paper1/labour/_meta.tsx
@@ -5,5 +5,6 @@ export default {
   "op-position": "OP Position",
   "deep-transverse-arrest": "Deep Transverse Arrest",
   "induction-of-labor": "Induction of Labor",
+  "instrumental-deliveries": "Instrumental Deliveries",
   "vacuum-delivery": "Vacuum Delivery",
 };
