5bdfa0bf0a55b4ddf94240d3fe6a01e27f3de922
whitedark9 <blancooscuro.mm@gmail.com>
2025-07-16 00:35:57 +0630

    up

--- src/pages/ob-gyn/theory/paper1/labour/_meta.tsx
+++ src/pages/ob-gyn/theory/paper1/labour/_meta.tsx
@@ -3,8 +3,7 @@ export default {
   "breech-presentation": "Breech Presentation",
   "transverse-lie": "Transverse Lie",
   "op-position": "OP Position",
-  "deep-transverse-arrest": "Deep Transverse Arrest",
   "induction-of-labor": "Induction of Labor",
   "instrumental-deliveries": "Instrumental Deliveries",
-  "vacuum-delivery": "Vacuum Delivery",
+  "normal-labour": "Normal Labour",
 };
--- src/pages/ob-gyn/theory/paper2/_meta.tsx
+++ src/pages/ob-gyn/theory/paper2/_meta.tsx
@@ -1,5 +1,6 @@
 export default {
   "disorders-of-menstruation": "Disorders of Menstruation",
   endometriosis: "Endometriosis",
+  "disorders-of-early-pregnancy": "Disorders of Early Pregnancy",
   menopause: "Menopause",
 };
