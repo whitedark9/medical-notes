9be67a45501a021cbb520ca01b81cc449addadce
whitedark9 <blancooscuro.mm@gmail.com>
2025-08-18 08:47:07 +0630

    up

--- src/pages/paediatrics/paper-1/_meta.tsx
+++ src/pages/paediatrics/paper-1/_meta.tsx
@@ -1,8 +1,8 @@
 export default {
   genetics: "Genetics",
   neonatology: "Neonatology",
-  // development: "Growth and Development",
-  // nutrition: "Nutrition",
-  // infection: "Infection",
-  // prescribing: "Pediatric Prescribing",
+  development: "Growth and Development",
+  nutrition: "Nutrition",
+  infection: "Infection",
+  paracetamol: "Paracetamol",
 };
---
+++ src/pages/paediatrics/paper-2/_meta.tsx
@@ -0,0 +1,8 @@
+export default {
+  cvs: "Cardiovascular System",
+  respiratory: "Respiratory System",
+  cns: "Central Nervous System",
+  gi: "Gastrointestinal System",
+  renal: "Renal System",
+  haematology: "Hematology",
+};
